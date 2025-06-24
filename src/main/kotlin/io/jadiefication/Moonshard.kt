91004b8498191dce7bda1ac374d15c801dd1c8ee
Jadiefication <jadeczjade008@gmail.com>
2025-06-24 19:22:42 +0200

    feat: setup base command

--- src/main/kotlin/io/jadiefication/Moonshard.kt
+++ src/main/kotlin/io/jadiefication/Moonshard.kt
@@ -1,2 +1,23 @@
 package io.jadiefication
 
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.parameters.options.help
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.prompt
+import com.github.ajalt.clikt.parameters.types.choice
+import io.jadiefication.minecraft.Versions
+
+class Moonshard: CliktCommand() {
+
+    private val options = Versions.entries.map { it.name.lowercase() }.toTypedArray()
+    val version = option("--version", "-v").choice(*options).prompt("Minecraft version").help {
+        return@help "The minecraft version of the mods"
+    }
+
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+}
+
+fun main(args: Array<String>) = Moonshard().main(args)
